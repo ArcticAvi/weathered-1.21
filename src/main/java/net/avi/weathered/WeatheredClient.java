6af72a425a6109c8a8be2d78bdbb46d3d3dc716a
Aviitori <leovaldz97@gmail.com>
2026-08-09 12:44:01 -0400

    testing

---
+++ src/main/java/net/avi/weathered/WeatheredClient.java
@@ -0,0 +1,10 @@
+package net.avi.weathered;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeatheredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
